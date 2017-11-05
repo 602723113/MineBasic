179a60a81aa11b724465f8ebe0485d66c5657d20
Zoyn <602723113@qq.com>
2017-11-05 19:07:38 +0800

    feat: add ModuleClassLoader,modified MineBasic#loadModules

--- src/main/java/cc/zoyn/minebasic/MineBasic.java
+++ src/main/java/cc/zoyn/minebasic/MineBasic.java
@@ -3,6 +3,8 @@ package cc.zoyn.minebasic;
 import cc.zoyn.minebasic.exception.UnLoadableModuleException;
 import cc.zoyn.minebasic.manager.ModuleManager;
 import cc.zoyn.minebasic.plugin.Module;
+import cc.zoyn.minebasic.plugin.ModuleClassLoader;
+import cc.zoyn.minebasic.plugin.ModuleInformation;
 import cc.zoyn.minebasic.util.FileNameFilter;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -10,7 +12,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.InputStreamReader;
+import java.lang.reflect.Method;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Set;
 
 /**
@@ -69,14 +73,16 @@ public class MineBasic extends JavaPlugin {
         if (moduleJars == null)
             return;
 
+        URL url;
         InputStreamReader inputStreamReader = null;
         FileConfiguration fileConfiguration;
-        
-        Class<?> moduleClass;
+
+        String className;
+        Class<? extends Module> moduleClass;
         Module module = null;
-        for (int i = 0; i < moduleJars.length; i++) {
+        for (File moduleJar : moduleJars) {
             try {
-                URL url = new URL("jar:file:" + moduleJars[i].getAbsolutePath() + "!/module.yml");
+                url = new URL("jar:file:/" + moduleJar.getAbsolutePath() + "!/module.yml");
                 inputStreamReader = new InputStreamReader(url.openStream());
             } catch (Exception e) {
                 e.printStackTrace();
@@ -86,7 +92,7 @@ public class MineBasic extends JavaPlugin {
                 throw new UnLoadableModuleException("can't load module.yml!");
             }
 
-            // check module.yml datas
+            // check module.yml data
             fileConfiguration = YamlConfiguration.loadConfiguration(inputStreamReader);
             Set<String> keys = fileConfiguration.getKeys(false);
             if (!keys.contains("name"))
@@ -98,15 +104,30 @@ public class MineBasic extends JavaPlugin {
             if (!keys.contains("author"))
                 throw new UnLoadableModuleException("module.yml doesn't have author!");
 
-            
+
             try {
-                moduleClass = Class.forName(fileConfiguration.getString("main"));
+                ModuleInformation moduleInformation = new ModuleInformation(fileConfiguration.getString("name"), fileConfiguration.getString("main"), fileConfiguration.getString("version"), fileConfiguration.getString("author"));
+                className = moduleInformation.getMain();
 
-                ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-                classLoader.loadClass(moduleClass.getName());
+                // load class...
+//                Method method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+//                method.setAccessible(true);
 
-                System.out.println(moduleClass.getName());
-                module = (Module) moduleClass.newInstance();
+                // 获取系统类加载器
+                ModuleClassLoader moduleClassLoader = ModuleClassLoader.getInstance();
+                moduleClassLoader.loadJar(moduleJar.toURI().toURL());
+
+//                URLClassLoader classLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+//                method.invoke(classLoader, moduleJar.toURI().toURL());
+
+                System.out.println("加载成功");
+                Class classObj = Class.forName("cc.zoyn.spawn.Spawn");
+                classObj.getMethod("onLoad").invoke(classObj);
+//                moduleClass = Class.forName(className).asSubclass(Module.class);
+
+
+//                moduleClass = Class.forName("cc.zoyn.spawn.Spawn", true, classLoader);
+//                module = (Module) moduleClass.newInstance();
             } catch (Exception e) {
                 e.printStackTrace();
             }
---
+++ src/main/java/cc/zoyn/minebasic/plugin/ModuleClassLoader.java
@@ -0,0 +1,48 @@
+package cc.zoyn.minebasic.plugin;
+
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+/**
+ * To load a module's jar
+ *
+ * @author Zoyn
+ * @since 2017-11-05
+ */
+public class ModuleClassLoader extends URLClassLoader {
+
+    private static ModuleClassLoader instance;
+    private static URLClassLoader classLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+    private static final Method ADD_URL = initAddMethod();
+
+    private ModuleClassLoader(URL[] urls) {
+        super(urls);
+    }
+
+    public static ModuleClassLoader getInstance() {
+        if (instance == null) {
+            instance = new ModuleClassLoader(new URL[]{});
+        }
+        return instance;
+    }
+
+    private static Method initAddMethod() {
+        try {
+            Method addUrl = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+            addUrl.setAccessible(true);
+            return addUrl;
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void loadJar(URL url) {
+        try {
+            ADD_URL.invoke(classLoader, url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
